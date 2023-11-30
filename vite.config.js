a0edcd4476330dcfeed7c4d99de466629125aa7a
Rob <dev@robrotell.com>
2023-11-29 23:29:49 -0500

    Adding custom resolve

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import browserslistToEsbuild from 'browserslist-to-esbuild'
+import path from 'path'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [ preact() ],
+	build: {
+		target: browserslistToEsbuild(),
+	},
+	resolve: {
+		alias: {
+		  '@': path.resolve( __dirname, 'src' ),
+		}
+	  }
 })
