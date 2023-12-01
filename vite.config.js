bbaab27364fff97c7bce89ae5ac151b45194a79c
Rob <dev@robrotell.com>
2023-11-30 23:23:04 -0500

    HTML minification

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,17 @@ import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import browserslistToEsbuild from 'browserslist-to-esbuild'
 import path from 'path'
-
+import { ViteMinifyPlugin } from 'vite-plugin-minify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [ preact() ],
+	plugins: [preact(), ViteMinifyPlugin({})],
 	build: {
 		target: browserslistToEsbuild(),
 	},
 	resolve: {
 		alias: {
-		  '@': path.resolve( __dirname, 'src' ),
-		}
-	  }
+			'@': path.resolve(__dirname, 'src'),
+		},
+	},
 })
